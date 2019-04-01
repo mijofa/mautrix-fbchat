f9fdb8b610c42d767b1e69f10219c9e6338cef6a
Mike Abrahall <mijofa-git@abrahall.id.au>
2019-04-02 10:03:00 +1100

    Refactor most of the registration code for more repeated usage

--- register.py
+++ register.py
@@ -1,50 +1,105 @@
 #!/usr/bin/python3
+import argparse
 import getpass
+import os.path
 import secrets
+import sys
 
 import yaml
 
 import fbchat
 
 
-appservice_port = 8091  # FIXME: Make this more dynamic, or at least configurable with a command-line argument
+argparser = argparse.ArgumentParser(
+    description="Log in to Facebook chat and create a registration yaml config file for Synapse and mautrix-fbchat to use")
+argparser.add_argument(
+    'yaml_filename',
+    nargs="?",
+    help=("Filename to save the yaml config to."
+          "If file already exists, just refresh the Facebook session cookies"))
+argparser.add_argument(
+    '-u', '--username',
+    help="Facebook username to log in with")
+argparser.add_argument(
+    '-p', '--port',
+    default=8091, type=int,
+    help="Port number to configure the app service listener to run on")
+args = argparser.parse_args()
 
 
-fb_username = input("Facebook username, email address, or phone number: ")
-fb_password = getpass.getpass("Facebook password (will not echo): ")
-print("Logging into Facebook now.  If prompted for 2FA code, you can give an empty code if you accept the notification prompts from a logged in session")
-fb = fbchat.Client(
-    email=fb_username,
-    password=fb_password)
+if args.yaml_filename and os.path.isfile(yaml_filename):
+    with open(args.yaml_filename, 'r') as f:
+        registration_data = yaml.load(f)
 
+    print("Only updating Facebook session cookies because config file already exists", sys.stderr)
 
-registration_data = {
-    'id': f"fbchat bridge for {fb.uid}",
-    'url': f"http://127.0.0.1:{appservice_port}",  # FIXME: Is this actually needed?
-    'hs_token': secrets.token_hex(),  # Does this follow the same spec as Matrix expects?
-    'as_token': secrets.token_hex(),  # Perhaps it could be more random?
-    'namespaces': {
-        # Yes these are single-item lists, it's not a typo, Synapse actually needs it to work that way.
-        'users': [{
-            'exclusive': True,
-            'regex': f"@fbchat_{fb.uid}_*",
-        }],
-        'aliases': [{
-            'exclusive': True,
-            'regex': f"#fbchat_{fb.uid}_*",
-        }],
-        'rooms': []  # FIXME: Can I get away with just removing this?
-    },
-    'sender_localpart': f"fbchat_{fb.uid}",
-    'rate_limited': True,  # FIXME: Facebook does rate limiting internally, but good to have this in case I fuck up the Python code
-    'protocols': ["fbchat"],
+    if args.username and args.username != registration_data['fbchat_username']:
+        raise Exception("Requested Facebook username doesn't match config")
+    fb_session = registration_data['fbchat_session']
+else:
+    registration_data = None
 
-    # I would like to avoid having multiple config files sharing some of the same secret tokens.
-    # So instead of copying the as/hs_token values into another config file along with the Facebook auth tokens,
-    # I'm just going include the Facebook auth tokens in with the Synapse config and rely on Synapse ignoring it.
-    'fbchat_session': fb.getSession(),  # FIXME: Do I need the whole thing? Is this secure enough?
-}
-with open(f"fbchat_{fb.uid}_appservice.yaml", 'w') as f:
+    print("If prompted for 2FA code, you can give an empty code after approving the login from a logged in session")
+    fb_username = input("Facebook username, email address, or phone number: ")
+    fb_session = {}
+
+try:
+    fb = fbchat.Client(
+        email=fb_username,
+        password='?',  # Try using the session cookies before actually asking the user for a password
+        session_cookies=fb_session,
+        max_tries=1,
+    )
+except fbchat._exception.FBchatUserError as e:
+    if e.args[0].startswith('Login failed. Check email/password'):
+        fb = fbchat.Client(
+            email=fb_username,
+            password=getpass.getpass("Facebook password (will not echo): "),
+            session_cookies=fb_session,
+        )
+    else:
+        raise
+
+if registration_data:
+    if registration_data['fbchat_uid'] != fb.uid:
+        raise fbchat._exception.FBchatUserError("Logged in Facebook account doesn't match config")
+
+    registration_data['fbchat_session'] = fb.getSession()
+    # Since the port number is configurable via command-line, I should update that too.
+    registration_data['url'] = f"http://127.0.0.1:{args.port}"
+else:
+    registration_data = {
+        'id': f"fbchat bridge for Facebook user {fb.uid}",
+        'url': f"http://127.0.0.1:{args.port}",  # FIXME: Is this actually needed?
+        'hs_token': secrets.token_hex(),  # Does this follow the same spec as Matrix expects?
+        'as_token': secrets.token_hex(),  # Perhaps it could be more random?
+        'namespaces': {
+            # Yes these are single-item lists, it's not a typo, Synapse actually needs it to work that way.
+            'users': [
+                {'exclusive': True, 'regex': f"@fbchat_{fb.uid}_*"}
+            ],
+            'aliases': [
+                {'exclusive': True, 'regex': f"#fbchat_{fb.uid}_*"}
+            ],
+            'rooms': []  # FIXME: Can I get away with just removing this?
+        },
+        'sender_localpart': f"fbchat_{fb.uid}",
+        'rate_limited': True,
+        'protocols': ["fbchat"],
+
+        # I would like to avoid having multiple config files sharing some of the same secret tokens.
+        # So instead of copying the as/hs token values into another config file along with the Facebook auth tokens,
+        # I'm just going include the Facebook auth tokens in with the Synapse config and rely on Synapse ignoring it.
+        'fbchat_username': fb.email,  # fbchat library won't let me login with *just* the session cookies, so use the username too.
+        'fbchat_uid': fb.uid,  # Just so we can later confirm who this config
+        'fbchat_session': fb.getSession(),  # FIXME: Do I need all the session cookies? Is this yaml file stored securely?
+    }
+
+fb.logout()  # Probably happens automatically with garbage collection, but can't hurt
+
+yaml_filename = args.yaml_filename if args.yaml_filename else f"fbchat_{fb.uid}_appservice.yaml",
+with open(yaml_filename.format(**registration_data), 'w') as f:
     yaml.dump(registration_data, f)
+    output_filename = f.name
 
-print(f"\n\nNow Copy {f.name} into a more sensible place and append it to Synapse's app_service_config_files option in homeserver.yaml\n")
+print(f"\n\Copy {f.name} into a more sensible place and add to Synapse's app_service_config_files option\n")
