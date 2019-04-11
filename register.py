cabc4ba97a3a9f07aa480d55f298917cc4e007fd
Mike Abrahall <mike@abrahall.id.au>
2019-04-11 17:24:20 +1000

    Tidy up registration py

--- register.py
+++ register.py
@@ -18,12 +18,21 @@ argparser.add_argument(
     help=("Filename to save the yaml config to."
           "If file already exists, just refresh the Facebook session cookies"))
 argparser.add_argument(
-    '-u', '--username',
-    help="Facebook username to log in with")
+    '-m', '--matrix-user',
+    required=True,
+    help="Username of the local Matrix user to bridge with Facebook (localpart only)")
+argparser.add_argument(
+    '-f', '--fb-user',
+    required=True,
+    help="Facebook email address, username, or phone number to log in with")
 argparser.add_argument(
     '-p', '--port',
     default=8091, type=int,
     help="Port number to configure the app service listener to run on")
+argparser.add_argument(
+    '-b', '--baseurl',
+    default='http://127.0.0.1:8008',
+    help="Matrix server base URL")
 args = argparser.parse_args()
 
 
@@ -33,10 +42,10 @@ if args.yaml_filename and os.path.isfile(args.yaml_filename):
 
     print("Only updating Facebook session cookies because config file already exists", file=sys.stderr)
 
-    if args.username and args.username != registration_data['fbchat_username']:
+    if args.fb_user and args.fb_user != registration_data['fbchat_username']:
         raise Exception("Requested Facebook username doesn't match config")
-    elif not args.username:
-        args.username = registration_data['fbchat_username']
+    elif not args.fb_user:
+        args.fb_user = registration_data['fbchat_username']
     fb_session = registration_data['fbchat_session']
 else:
     registration_data = None
@@ -44,12 +53,12 @@ else:
     print("If prompted for 2FA code, you can give an empty code after approving the login from a logged in session")
     fb_session = {}
 
-args.username = input("Facebook username, email address, or phone number: ") if not args.username else args.username
+args.fb_user = input("Facebook username, email address, or phone number: ") if not args.fb_user else args.fb_user
 
 
 try:
     fb = fbchat.Client(
-        email=args.username,
+        email=args.fb_user,
         password='?',  # Try using the session cookies before actually asking the user for a password
         session_cookies=fb_session,
         max_tries=1,
@@ -57,7 +66,7 @@ try:
 except fbchat._exception.FBchatUserError as e:
     if e.args[0].startswith('Login failed. Check email/password'):
         fb = fbchat.Client(
-            email=args.username,
+            email=args.fb_user,
             password=getpass.getpass("Facebook password (will not echo): "),
             session_cookies=fb_session,
         )
@@ -67,40 +76,44 @@ except fbchat._exception.FBchatUserError as e:
 if registration_data:
     if registration_data['fbchat_uid'] != fb.uid:
         raise fbchat._exception.FBchatUserError("Logged in Facebook account doesn't match config")
-
-    registration_data['fbchat_session'] = fb.getSession()
-    # Since the port number is configurable via command-line, I should update that too.
-    registration_data['url'] = f"http://127.0.0.1:{args.port}"
+    hs_token = registration_data['hs_token']
+    as_token = registration_data['as_token']
 else:
-    registration_data = {
-        'id': f"fbchat bridge for Facebook user {fb.uid}",
-        'url': f"http://127.0.0.1:{args.port}",  # FIXME: Is this actually needed?
-        'hs_token': secrets.token_hex(),  # Does this follow the same spec as Matrix expects?
-        'as_token': secrets.token_hex(),  # Perhaps it could be more random?
-        'namespaces': {
-            # Yes these are single-item lists, it's not a typo, Synapse actually needs it to work that way.
-            'users': [
-                {'exclusive': True, 'regex': f"@fbchat_{fb.uid}_*"}
-            ],
-            'aliases': [
-                {'exclusive': True, 'regex': f"#fbchat_{fb.uid}_*"}
-            ],
-            'rooms': []  # FIXME: Can I get away with just removing this?
-        },
-        'sender_localpart': f"fbchat_{fb.uid}",
-        'rate_limited': True,
-        'protocols': ["fbchat"],
+    hs_token = secrets.token_hex(),  # Does this follow the same spec as Matrix expects?
+    as_token = secrets.token_hex(),  # Perhaps it could be more random?
 
-        # I would like to avoid having multiple config files sharing some of the same secret tokens.
-        # So instead of copying the as/hs token values into another config file along with the Facebook auth tokens,
-        # I'm just going include the Facebook auth tokens in with the Synapse config and rely on Synapse ignoring it.
-        'fbchat_username': fb.email,  # fbchat library won't let me login with *just* the session cookies, so use the username too.
-        'fbchat_uid': fb.uid,  # Just so we can later confirm who this config
-        'fbchat_session': fb.getSession(),  # FIXME: Do I need all the session cookies? Is this yaml file stored securely?
-    }
+registration_data = {
+    'id': f"mautrix-fbchat_{fb.uid}",
+    'url': f"http://127.0.0.1:{args.port}",  # FIXME: Is this actually needed?
+    'hs_token': hs_token,
+    'as_token': as_token,
+    'namespaces': {
+        # Yes these are single-item lists, it's not a typo, Synapse actually needs it to work that way.
+        'users': [
+            {'exclusive': True, 'regex': f"@fbchat_{fb.uid}_.*"}
+        ],
+        'aliases': [
+            {'exclusive': True, 'regex': f"#fbchat_{fb.uid}_.*"}
+        ],
+        'rooms': []  # FIXME: Can I get away with just removing this?
+    },
+    'sender_localpart': f"fbchat_{fb.uid}",
+    'rate_limited': True,
+    'protocols': ["fbchat"],
+
+    # I would like to avoid having multiple config files sharing some of the same secret tokens.
+    # So instead of copying the as/hs token values into another config file along with the Facebook auth tokens,
+    # I'm just going include the Facebook auth tokens in with the Synapse config and rely on Synapse ignoring it.
+    'fbchat_username': fb.email,  # fbchat library won't let me login with *just* the session cookies, so use the username too.
+    'fbchat_uid': fb.uid,  # Just so we can later confirm who this config
+    'fbchat_session': fb.getSession(),  # FIXME: Do I need all the session cookies? Is this yaml file stored securely?
+
+    'matrix_baseurl': args.baseurl,
+    'matrix_user_localpart': args.matrix_user,
+}
 
 yaml_filename = args.yaml_filename if args.yaml_filename else f"fbchat_{fb.uid}_appservice.yaml"
-with open(yaml_filename.format(**registration_data), 'w') as f:
+with open(yaml_filename, 'w') as f:
     yaml.dump(registration_data, f)
     output_filename = f.name
 
